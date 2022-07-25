80c17bb612474b931629c8ab912d9e6e287009b5
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-24 22:35:10 -0700

    added the comments, but there is still some sort of confusion, idk why i am having hard time here in getting this question, probably I need to go to the deep understanding of recursion more to get it, I will do that.

--- src/CountSingleValuedSubTree.java
+++ src/CountSingleValuedSubTree.java
@@ -1,3 +1,4 @@
+//tree node
 class Node {
     int data;
     Node left;
@@ -11,27 +12,33 @@ class Node {
 
 public class CountSingleValuedSubTree {
 
+
     static int count;
+    //get a recursive call method which is basically working as a postorder traversal
     static boolean rec(Node root){
         if(root == null) return true;
 
+        //store the results of left and right node
         boolean ls = rec(root.left);
         boolean rs = rec(root.right);
 
+        //if either of the result is false, then return false
         if(ls== false || rs== false ){
             return false;
         }
+        //if its side element is not null and not having similar data then return false
         if(root.left != null && root.left.data != root.data){
             return false;
         }
         if(root.right != null && root.right.data != root.data){
             return false;
         }
+        //otherwise just increment the count and return true
         count ++;
         return true;
     }
     static int countSingleValuedSubTree(Node root){
-
+        //apply the rec method approach to count the number of singlevalued subtreees
         count =0;
         rec(root);
         return count;
