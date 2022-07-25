2ba745ce48ed1249786040ac04eef7a1871d4e90
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-24 22:20:44 -0700

    coded a node class

--- src/CountSingleValuedSubTree.java
+++ src/CountSingleValuedSubTree.java
@@ -1,5 +1,15 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data, Node left, Node right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class CountSingleValuedSubTree {
-    
 
 
     public static void main(String[] args) {
