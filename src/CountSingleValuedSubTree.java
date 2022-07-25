c0527504e433950d7d2883f662736a422fbf093a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-24 22:25:29 -0700

    coded a recursive method to implement hteh single valued calls

--- src/CountSingleValuedSubTree.java
+++ src/CountSingleValuedSubTree.java
@@ -11,6 +11,27 @@ class Node {
 
 public class CountSingleValuedSubTree {
 
+    static int count;
+    static boolean rec(Node root){
+        if(root == null) return true;
+
+        boolean ls = rec(root.left);
+        boolean rs = rec(root.right);
+
+        if(ls== false || rs== false ){
+            return false;
+        }
+        if(root.left != null && root.left.data != root.data){
+            return false;
+        }
+        if(root.right != null && root.right.data != root.data){
+            return false;
+        }
+        count ++;
+        return true;
+    }
+
+
 
     public static void main(String[] args) {
 
